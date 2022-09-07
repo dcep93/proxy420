dec2c38efc00f902bb6cab6da1bed94431637699
dcep93 <dcep93@gmail.com>
2022-09-07 16:28:08 -0400

    x

--- .github/workflows/deploy_to_app_engine.sh
+++ .github/workflows/deploy_to_app_engine.sh
@@ -13,8 +13,7 @@ set -euo pipefail
 cd app
 export GOOGLE_APPLICATION_CREDENTIALS="gac.json"
 echo "$1" > "$GOOGLE_APPLICATION_CREDENTIALS"
-npm install gcloud
-exit 0
+npm install google-cloud
 gcloud auth activate-service-account --key-file="$GOOGLE_APPLICATION_CREDENTIALS"
 echo "runtime: nodejs16" > app.yaml
 gcloud app deploy

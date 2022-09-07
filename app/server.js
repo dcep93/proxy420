202fbc62728dac3767719ccc563571a75b693b97
dcep93 <dcep93@gmail.com>
2022-09-07 18:09:32 -0400

    x

--- app/server.js
+++ app/server.js
@@ -1,17 +1,19 @@
-var http = require("http");
-var url = require("url");
+var express = require("express");
+var bodyParser = require("body-parser");
 var fetch = require("node-fetch");
 
 var port = process.env.PORT;
 
-http
-  .createServer(function (req, res) {
-    const queryObject = url.parse(req.url, true).query;
-    fetch(queryObject.url)
-      .then((resp) => resp.text())
-      .then((text) => {
-        res.write(text);
-        res.end();
-      });
-  })
-  .listen(port);
+var app = express();
+app.use(bodyParser.json());
+
+app.post("/", (req, res) =>
+  Promise.resolve(req.body)
+    .then((body) => body.url)
+    .then(fetch(url))
+    .then((resp) => resp.text())
+    .then((text) => res.send(text))
+    .catch((err) => res.status(500).send(err))
+);
+
+app.listen(port);

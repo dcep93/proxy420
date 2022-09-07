fc5df939e7423ea69f0e0abf5eb1e56b237bbe39
dcep93 <dcep93@gmail.com>
2022-09-07 16:20:28 -0400

    x

---
+++ .github/workflows/deploy_to_app_engine.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -euo pipefail
+
+# # disable billing
+# # create firebase project
+# nvm install 16.4.0
+# gcloud app create --project "$GOOGLE_CLOUD_PROJECT" --region us-east1
+# gcloud iam service-accounts create deployer-github
+# gcloud projects add-iam-policy-binding "$GOOGLE_CLOUD_PROJECT" --member="serviceAccount:deployer-github@$GOOGLE_CLOUD_PROJECT.iam.gserviceaccount.com" --role="roles/appengine.deployer"
+# gcloud iam service-accounts keys create gac.json --iam-account "deployer-github@$GOOGLE_CLOUD_PROJECT.iam.gserviceaccount.com"
+# cat gac.json
+
+cd app
+export GOOGLE_APPLICATION_CREDENTIALS="gac.json"
+echo "$1" > "$GOOGLE_APPLICATION_CREDENTIALS"
+npm install gcloud
+gcloud auth activate-service-account --key-file="$GOOGLE_APPLICATION_CREDENTIALS"
+gcloud app deploy

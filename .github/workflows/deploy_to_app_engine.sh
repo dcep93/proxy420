46749fa7790fc4678720a52a0d4d313352b77f44
dcep93 <dcep93@gmail.com>
2022-09-07 16:47:54 -0400

    x

--- .github/workflows/deploy_to_app_engine.sh
+++ .github/workflows/deploy_to_app_engine.sh
@@ -3,6 +3,7 @@
 set -euo pipefail
 
 # # enable api https://console.developers.google.com/apis/api/appengine.googleapis.com/overview
+# # enable cloud build https://console.cloud.google.com/apis/library/cloudbuild.googleapis.com
 # # disable billing
 # nvm install 16.4.0
 # gcloud app create --project "$GOOGLE_CLOUD_PROJECT" --region us-east1

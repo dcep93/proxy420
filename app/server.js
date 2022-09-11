73f8b9611fbd03e09fe8d431af490e576c6e9870
dcep93 <dcep93@gmail.com>
2022-09-11 17:11:06 -0400

    x

--- app/server.js
+++ app/server.js
@@ -31,48 +31,45 @@ app.post("/", (req, res) =>
     })
 );
 
-app.get(
-  "/*",
-  (req, res) =>
-    Promise.resolve({
-      path: req.params[0],
-      query: Object.entries(req.query)
-        .map(([k, v]) => [k, v].join("="))
-        .join("&"),
+app.get("/*", (req, res) =>
+  Promise.resolve({
+    path: req.params[0],
+    query: Object.entries(req.query)
+      .map(([k, v]) => [k, v].join("="))
+      .join("&"),
+  })
+    .then(({ path, query }) => `https://tinyurl.is/${path}?${query}`)
+    .then((url) =>
+      fetch(url, {
+        headers: {
+          accept:
+            "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+          "accept-language": "en-US,en;q=0.9",
+          "cache-control": "no-cache",
+          pragma: "no-cache",
+          "sec-ch-ua":
+            '"Google Chrome";v="105", "Not)A;Brand";v="8", "Chromium";v="105"',
+          "sec-ch-ua-mobile": "?0",
+          "sec-ch-ua-platform": '"macOS"',
+          "sec-fetch-dest": "document",
+          "sec-fetch-mode": "navigate",
+          "sec-fetch-site": "none",
+          "sec-fetch-user": "?1",
+          "upgrade-insecure-requests": "1",
+        },
+        referrerPolicy: "strict-origin-when-cross-origin",
+        body: null,
+        method: "GET",
+        mode: "cors",
+        credentials: "include",
+      })
+    )
+    .then((resp) => resp.text())
+    .then((text) => res.send(text))
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send(err);
     })
-      .then(({ path, query }) => `https://tinyurl.is/${path}?${query}`)
-      .then((resp) => res.send(resp))
-  // .then((url) =>
-  //   fetch(url, {
-  //     headers: {
-  //       accept:
-  //         "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-  //       "accept-language": "en-US,en;q=0.9",
-  //       "cache-control": "no-cache",
-  //       pragma: "no-cache",
-  //       "sec-ch-ua":
-  //         '"Google Chrome";v="105", "Not)A;Brand";v="8", "Chromium";v="105"',
-  //       "sec-ch-ua-mobile": "?0",
-  //       "sec-ch-ua-platform": '"macOS"',
-  //       "sec-fetch-dest": "document",
-  //       "sec-fetch-mode": "navigate",
-  //       "sec-fetch-site": "none",
-  //       "sec-fetch-user": "?1",
-  //       "upgrade-insecure-requests": "1",
-  //     },
-  //     referrerPolicy: "strict-origin-when-cross-origin",
-  //     body: null,
-  //     method: "GET",
-  //     mode: "cors",
-  //     credentials: "include",
-  //   })
-  // )
-  // .then((resp) => resp.text())
-  // .then((text) => res.send(text))
-  // .catch((err) => {
-  //   console.error(err);
-  //   res.status(500).send(err);
-  // })
 );
 
 app.listen(port);

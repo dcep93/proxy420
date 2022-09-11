9975ba97a9eec69edcf313c41f1937731ae30f35
dcep93 <dcep93@gmail.com>
2022-09-11 16:54:11 -0400

    x

--- app/server.js
+++ app/server.js
@@ -31,6 +31,41 @@ app.post("/", (req, res) =>
     })
 );
 
+app.get("/test", (req, res) =>
+  fetch(
+    "https://tinyurl.is/cdn-cgi/challenge-platform/h/g/orchestrate/jsch/v1?ray=749349e39af86fe9",
+    {
+      headers: {
+        accept:
+          "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+        "accept-language": "en-US,en;q=0.9",
+        "cache-control": "no-cache",
+        pragma: "no-cache",
+        "sec-ch-ua":
+          '"Google Chrome";v="105", "Not)A;Brand";v="8", "Chromium";v="105"',
+        "sec-ch-ua-mobile": "?0",
+        "sec-ch-ua-platform": '"macOS"',
+        "sec-fetch-dest": "document",
+        "sec-fetch-mode": "navigate",
+        "sec-fetch-site": "none",
+        "sec-fetch-user": "?1",
+        "upgrade-insecure-requests": "1",
+      },
+      referrerPolicy: "strict-origin-when-cross-origin",
+      body: null,
+      method: "GET",
+      mode: "cors",
+      credentials: "include",
+    }
+  )
+    .then((resp) => resp.text())
+    .then((text) => res.send(text))
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send(err);
+    })
+);
+
 app.get("/:url*", (req, res) =>
   Promise.resolve(req.params.url)
     .then((url) => `https://tinyurl.is/${url}`)

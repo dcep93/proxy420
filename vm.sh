19000337384b71004c7dcbc679ad6e6c54ba3321
dcep93 <dcep93@gmail.com>
2022-09-11 23:00:50 -0400

    x

--- vm.sh
+++ vm.sh
@@ -5,5 +5,5 @@ sudo apt-get install npm
 git clone https://github.com/dcep93/proxy420
 cd proxy420/app
 screen
-git reset --hard HEAD && git pull && npm install && PORT=8080 npm start
+git reset --hard HEAD && git pull && npm install && printf "<pre>%s\n%s\n</pre>\n" "$(TZ='America/Los_Angeles' date)" "$(git log -1)" > recorded_sha.txt && PORT=8080 npm start
 # git pull

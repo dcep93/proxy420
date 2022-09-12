1c8456a993f6f897a0d156788d896f053ab2645d
dcep93 <dcep93@gmail.com>
2022-09-11 22:16:23 -0400

    x

--- app/main.py
+++ app/main.py
@@ -2,14 +2,26 @@ import requests
 
 from flask import Flask
 
-# If `entrypoint` is not defined in app.yaml, App Engine will look for an app
-# called `app` in `main.py`.
 app = Flask(__name__)
 
 
 @app.route('/')
 def ipify():
     resp = requests.get("https://api.ipify.org?format=json")
+    return resp.text
+
+
+@app.route('/tinyurl')
+def tinyurl():
+    resp = requests.get("https://tinyurl.is/IDu0?sport=american-football")
+    return resp.text
+
+
+@app.route('/weakstreams')
+def weakstreams():
+    resp = requests.get(
+        "https://weakstreams.com/nfl-streams/detroit-lions-vs-philadelphia-eagles/81031?sport=american-football"
+    )
     return resp.text
 
 

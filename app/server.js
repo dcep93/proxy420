7f437256130254305f9c4db790c777d8b8504c0c
dcep93 <dcep93@gmail.com>
2022-09-11 21:38:32 -0400

    x

--- app/server.js
+++ app/server.js
@@ -31,6 +31,32 @@ app.post("/", (req, res) =>
     })
 );
 
+app.get("/tinyurl", (req, res) =>
+  Promise.resolve()
+    .then(() => fetch("https://tinyurl.is/IDu0?sport=american-football"))
+    .then((resp) => resp.text())
+    .then((text) => res.send(text))
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send(err);
+    })
+);
+
+app.get("/weakstreams", (req, res) =>
+  Promise.resolve()
+    .then(() =>
+      fetch(
+        "https://weakstreams.com/nfl-streams/detroit-lions-vs-philadelphia-eagles/81031?sport=american-football"
+      )
+    )
+    .then((resp) => resp.text())
+    .then((text) => res.send(text))
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send(err);
+    })
+);
+
 app.get("/*", (req, res) =>
   Promise.resolve({
     path: req.params[0],

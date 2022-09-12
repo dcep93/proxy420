06e01cacc4582e02027965dd20e371a86c8dbe11
dcep93 <dcep93@gmail.com>
2022-09-12 00:16:09 -0400

    x

--- app/server.js
+++ app/server.js
@@ -14,6 +14,8 @@ var hostname = os.hostname();
 
 var port = process.env.PORT;
 
+const VM_ADDR = "http://35.224.149.167";
+
 var app = express();
 app.use(bodyParser.json());
 app.use(cors());
@@ -21,6 +23,23 @@ app.use(cors());
 app.get("/", (req, res) => {
   res.send(recorded_sha);
 });
+
+app.get("/*", (req, res) =>
+  Promise.resolve({
+    path: req.params[0],
+    query: Object.entries(req.query)
+      .map(([k, v]) => [k, v].join("="))
+      .join("&"),
+  })
+    .then(({ path, query }) => `${path}?${query}`)
+    .then((url) => fetch(url))
+    .then((resp) => resp.text())
+    .then((text) => res.send(text))
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send(`${hostname}: ${err}`);
+    })
+);
 
 app.post("/", (req, res) =>
   Promise.resolve(req.body)
@@ -33,15 +52,17 @@ app.post("/", (req, res) =>
     })
 );
 
-app.get("/*", (req, res) =>
-  Promise.resolve({
-    path: req.params[0],
-    query: Object.entries(req.query)
-      .map(([k, v]) => [k, v].join("="))
-      .join("&"),
-  })
-    .then(({ path, query }) => `${path}?${query}`)
-    .then((url) => fetch(url))
+app.post("/proxy", (req, res) =>
+  Promise.resolve(req.body)
+    .then((body) =>
+      fetch(VM_ADDR, {
+        method: "POST",
+        body: JSON.stringify(body),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+    )
     .then((resp) => resp.text())
     .then((text) => res.send(text))
     .catch((err) => {

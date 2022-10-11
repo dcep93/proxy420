7afceedfb8fce58bfbb3cf4729952cad2e32e70c
dcep93 <dcep93@gmail.com>
2022-10-11 15:07:06 -0400

    x

--- .github/workflows/deploy_to_app_engine.sh
+++ .github/workflows/deploy_to_app_engine.sh
@@ -19,6 +19,5 @@ export GOOGLE_APPLICATION_CREDENTIALS="gac.json"
 echo "$1" > "$GOOGLE_APPLICATION_CREDENTIALS"
 npm install google-auth-library
 gcloud auth activate-service-account --key-file="$GOOGLE_APPLICATION_CREDENTIALS"
-echo "runtime: nodejs16" > app.yaml
 project_id="$(cat $GOOGLE_APPLICATION_CREDENTIALS | jq -r .project_id)"
 gcloud app deploy --project "${project_id}" --version 1

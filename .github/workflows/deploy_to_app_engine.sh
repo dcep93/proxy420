6affc89341be285c055b5cbd58b32d549caa229a
dcep93 <dcep93@gmail.com>
2024-06-04 21:37:10 -0400

    x

--- .github/workflows/deploy_to_app_engine.sh
+++ .github/workflows/deploy_to_app_engine.sh
@@ -20,7 +20,7 @@ export GOOGLE_APPLICATION_CREDENTIALS="gac.json"
 echo "$1" >"$GOOGLE_APPLICATION_CREDENTIALS"
 npm install google-auth-library
 gcloud auth activate-service-account --key-file="$GOOGLE_APPLICATION_CREDENTIALS"
-project_id="$(cat $GOOGLE_APPLICATION_CREDENTIALS | jq -r .project_id)"
-gcloud app deploy --project "${project_id}" --version 2
-
-# gsutil -m rm -r "gs://us.artifacts.${project_id}.appspot.com"
+GOOGLE_CLOUD_PROJECT="$(cat $GOOGLE_APPLICATION_CREDENTIALS | jq -r .project_id)"
+gcloud app deploy --project "${GOOGLE_CLOUD_PROJECT}" --version 1 --no-cache
+gsutil -m rm -r "gs://us.artifacts.${GOOGLE_CLOUD_PROJECT}.appspot.com"
+# gcloud beta app repair

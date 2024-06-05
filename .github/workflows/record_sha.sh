87d990d8626b2ee865c1a33bf66f7ce2bb243c4d
dcep93 <dcep93@gmail.com>
2024-06-04 20:07:32 -0400

    x

--- .github/workflows/record_sha.sh
+++ .github/workflows/record_sha.sh
@@ -6,4 +6,4 @@ filename=recorded_sha.txt
 
 cd app
 test -f "$filename"
-printf "<pre>%s\n%s\n</pre>\n" "$(TZ='America/Los_Angeles' date)" "$(git log -1)" > "$filename"
+printf "<pre>%s\n%s\n</pre>\n" "$(TZ='America/New_York' date)" "$(git log -1)" >"$filename"
